f2c228715d5c1ca703447b9d87c1452d5ac3b6ba
Marden Rafael <mardenrafaeldalmagro.gimenez@gmail.com>
2023-05-27 11:30:41 -0400

    chore: add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      white: "#F2F2F2",
+      black: "#0D0D0D",
+      orange: "#BF681B",
+      yellow: "#DBF288",
+      blue: "#2481A6",
     },
   },
   plugins: [],
-}
+};
