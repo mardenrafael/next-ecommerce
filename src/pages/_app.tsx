ca114559588ea79aab348c5daf0b187781abc48a
Marden Rafael <mardenrafaeldalmagro.gimenez@gmail.com>
2023-05-27 11:30:22 -0400

    feat: add meta tags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>E-commerce</title>
+        <meta name="description" content="A melhor loja de roupas do Brasil" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
