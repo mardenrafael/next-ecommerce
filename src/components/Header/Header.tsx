006d9dbad2f40815f0ecf8f76e891ca1544e5b5a
Marden Rafael <mardenrafaeldalmagro.gimenez@gmail.com>
2023-05-27 11:30:58 -0400

    refac: change component folder

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,16 @@
+import NavBar from "../NavBar/NavBar";
+
+export interface HeaderProps {
+  title: String;
+}
+
+export default function Header({ title }: HeaderProps) {
+  return (
+    <header className="flex flex-col items-center border border-black">
+      <NavBar />
+      <section className="border border-black w-full p-4 h-40 flex items-center justify-center">
+        <h1>{title}</h1>
+      </section>
+    </header>
+  );
+}
