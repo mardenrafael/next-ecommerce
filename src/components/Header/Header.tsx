5fc3627b8e6607fe2045a6222780cb608d63a742
Marden Rafael <mardenrafaeldalmagro.gimenez@gmail.com>
2023-05-31 13:47:13 -0400

    refac: removed border

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,9 +6,9 @@ export interface HeaderProps {
 
 export default function Header({ title }: HeaderProps): JSX.Element {
   return (
-    <header className="flex flex-col items-center border border-black">
+    <header className="flex flex-col items-center">
       <NavBar />
-      <section className="border border-black w-full p-4 h-40 flex items-center justify-center">
+      <section className="w-full p-4 h-40 flex items-center justify-center">
         <h1>{title}</h1>
       </section>
     </header>
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -2,13 +2,17 @@ import Image from "next/image";
 
 export default function NavBar() {
   return (
-    <nav className="border border-black w-full p-4 flex items-center justify-between">
-      <section className="border border-black flex items-center justify-center">
+    <nav className="w-full p-4 flex items-center justify-between">
+      <section className="flex items-center justify-center">
         <Image alt="Logo da sua loja" src={"/"} width={96} height={96} />
       </section>
-      <section className="border border-black min-w-fit w-96 max-w-xs h-full p-1 flex gap-2 justify-evenly">
-        <div>Início</div>
-        <div>Contato</div>
+      <section className="min-w-fit w-96 max-w-xs h-full p-1 flex gap-2 justify-evenly">
+        <div>
+          <button>Inicio</button>
+        </div>
+        <div>
+          <button>Contato</button>
+        </div>
       </section>
     </nav>
   );
