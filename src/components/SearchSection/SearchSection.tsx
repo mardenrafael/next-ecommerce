29db7e1f410ee205f4bec590b4826b1441c1ad70
Marden Rafael <mardenrafaeldalmagro.gimenez@gmail.com>
2023-05-31 13:47:36 -0400

    refac: removed border

--- src/components/SearchSection/SearchSection.tsx
+++ src/components/SearchSection/SearchSection.tsx
@@ -1,10 +1,18 @@
-import TextInput from "../TextInput/TextInput";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Input from "../Input/Input";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 export default function SearchSection() {
   return (
-    <section className="border border-black w-full h-16 p-2 flex items-center justify-center">
-      <TextInput icon={faSearch} />
-    </section>
+    <div className="flex items-center justify-center">
+      <FontAwesomeIcon icon={faSearch} />
+      <section className="w-fit h-16 p-2 flex items-center justify-center">
+        <Input
+          required={false}
+          type="text"
+          placeholder="Digite o nome do produto..."
+        />
+      </section>
+    </div>
   );
 }
