6952b896539ddc719de6b2404b58df350bd2c6d1
Marden Rafael <mardenrafaeldalmagro.gimenez@gmail.com>
2023-05-31 13:44:53 -0400

    feat: create Input component

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,28 @@
+import { HTMLInputTypeAttribute } from "react";
+
+export interface InputProps {
+  placeholder?: string;
+  id?: string;
+  required: boolean;
+  type: HTMLInputTypeAttribute;
+  name?: string;
+}
+
+export default function Input({
+  placeholder,
+  id,
+  required,
+  type,
+  name,
+}: InputProps) {
+  return (
+    <input
+      type={type}
+      id={id ? id : "input"}
+      name={name}
+      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+      placeholder={placeholder}
+      required={required}
+    />
+  );
+}
