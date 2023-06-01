0a6e044e8d08ed2f9cfa21fd2faca50437fa1c60
Marden Rafael <mardenrafaeldalmagro.gimenez@gmail.com>
2023-06-01 00:23:22 -0400

    refac: recreated navbar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,19 +1,51 @@
 import Image from "next/image";
+import Anchor from "../Anchor/Anchor";
 
 export default function NavBar() {
   return (
-    <nav className="w-full p-4 flex items-center justify-between">
-      <section className="flex items-center justify-center">
-        <Image alt="Logo da sua loja" src={"/"} width={96} height={96} />
-      </section>
-      <section className="min-w-fit w-96 max-w-xs h-full p-1 flex gap-2 justify-evenly">
-        <div>
-          <button>Inicio</button>
+    <nav className="bg-white border-gray-200 dark:bg-gray-900">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <Anchor href="https://flowbite.com/" className="flex items-center">
+          <Image
+            src="https://flowbite.com/docs/images/logo.svg"
+            alt="logo"
+            className="h-8 mr-3"
+            width={32}
+            height={32}
+          />
+          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+            Flowbite
+          </span>
+        </Anchor>
+        <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <li>
+              <Anchor
+                href={"#"}
+                className="block py-2 pl-3 pr-4 text-white bg-primary-700 rounded md:bg-transparent md:text-primary-700 md:p-0 dark:text-white md:dark:text-primary-500"
+              >
+                Inicio
+              </Anchor>
+            </li>
+            <li>
+              <Anchor
+                href="#"
+                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-primary-700 md:p-0 dark:text-white md:dark:hover:text-primary-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              >
+                Produtos
+              </Anchor>
+            </li>
+            <li>
+              <Anchor
+                href="#"
+                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-primary-700 md:p-0 dark:text-white md:dark:hover:text-primary-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              >
+                Contato
+              </Anchor>
+            </li>
+          </ul>
         </div>
-        <div>
-          <button>Contato</button>
-        </div>
-      </section>
+      </div>
     </nav>
   );
 }
