e16c6b1834becfa425604b10ba2128d12eb55502
Marden Rafael <mardenrafaeldalmagro.gimenez@gmail.com>
2023-06-01 19:46:22 -0400

    refac: remake ProductCard

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,32 +1,44 @@
 import Image from "next/image";
+import applewatch from "../../../public/img/apple-watch.png";
+import Anchor from "../Anchor/Anchor";
 
 export interface ProductCardProps {
   productName: string;
-  productImage: string;
+  productImageUrl: string;
 }
 
 export default function ProductCard({
   productName,
-  productImage,
+  productImageUrl,
 }: ProductCardProps): JSX.Element {
   return (
-    <section className="flex flex-col items-center p-5">
-      <header className="flex items-center justify-center p-2 w-full">
-        <h1>{productName}</h1>
-      </header>
-      <main className="p-2 m-4">
+    <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+      <Anchor href="#" blankStyle>
         <Image
-          src={productImage}
-          alt={"Imagem do protudo"}
-          width={240}
-          height={240}
+          className="p-8 rounded-t-lg"
+          src={productImageUrl}
+          alt="product image"
+          width={500}
+          height={500}
         />
-      </main>
-      <footer className="w-full flex items-center justify-center p-2">
-        <main>
-          <button>Mais informações</button>
-        </main>
-      </footer>
-    </section>
+      </Anchor>
+      <div className="px-5 pb-5 flex flex-col gap-5">
+        <div className="flex items-center justify-center">
+          <Anchor href="#" blankStyle>
+            <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
+              {productName}
+            </h5>
+          </Anchor>
+        </div>
+        <div className="flex items-center justify-center">
+          <Anchor
+            href="#"
+            className="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+          >
+            Ver mais informações
+          </Anchor>
+        </div>
+      </div>
+    </div>
   );
 }
