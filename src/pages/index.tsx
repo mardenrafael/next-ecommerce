460211d1709654ead504da33777e55bf8171fc68
Marden Rafael <mardenrafaeldalmagro.gimenez@gmail.com>
2023-06-01 00:32:10 -0400

    feat: create server side props

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,36 @@
 import Footer from "@/components/Footer/Footer";
 import Header from "@/components/Header/Header";
-import HorizontalList from "@/components/HorizontalList/HorizontalList";
+import HorizontalList, {
+  HorizontalListProps,
+} from "@/components/HorizontalList/HorizontalList";
+import NavBar from "@/components/NavBar/NavBar";
 import SearchSection from "@/components/SearchSection/SearchSection";
+import { GetServerSidePropsResult } from "next";
 
-export default function Home() {
+export interface HomeProps extends HorizontalListProps {}
+
+export async function getServerSideProps(): Promise<
+  GetServerSidePropsResult<HorizontalListProps>
+> {
+  return {
+    props: {
+      products: [
+        {
+          productName: "produto server side",
+          productImage: "/",
+        },
+      ],
+    },
+  };
+}
+
+export default function Home({ products }: HomeProps) {
   return (
     <main className="bg-white">
+      <NavBar />
       <Header title={"E commerce"} />
       <SearchSection />
-      <HorizontalList />
+      <HorizontalList products={products} />
       <Footer />
     </main>
   );
