6966f12e4fe042b0b56d93ddb5805a9c40d388a8
Marden Rafael <mardenrafaeldalmagro.gimenez@gmail.com>
2023-06-01 19:46:03 -0400

    refac: using Product model

--- src/components/HorizontalList/HorizontalList.tsx
+++ src/components/HorizontalList/HorizontalList.tsx
@@ -1,26 +1,27 @@
-import ProductCard, { ProductCardProps } from "../ProductCard/ProductCard";
+import { Product } from "@prisma/client";
+import ProductCard from "../ProductCard/ProductCard";
 
 export interface HorizontalListProps {
-  products: ProductCardProps[]; // TODO: Achar uma representação melhro para product
+  products: Product[];
 }
 export default function HorizontalList({
   products,
 }: HorizontalListProps): JSX.Element {
+  function renderCards(): JSX.Element[] {
+    return [
+      <ProductCard
+        productName={products[0].name}
+        productImageUrl={products[0].image}
+      />,
+    ];
+  }
+
   return (
     <section className="p-4">
       <header className="p-2 font-bold text-4xl">
         <h1>Destaques</h1>
       </header>
-      <main className="grid grid-cols-4 gap-4">
-        {products.map((item) => {
-          return (
-            <ProductCard
-              productName={item.productName}
-              productImage={item.productImage}
-            />
-          );
-        })}
-      </main>
+      <main className="grid grid-cols-4 gap-4">{renderCards()}</main>
     </section>
   );
 }
