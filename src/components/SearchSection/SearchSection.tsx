3fad73ec074e4a1a2f13b8938166aadea6e281be
Marden Rafael <mardenrafaeldalmagro.gimenez@gmail.com>
2023-06-01 19:46:53 -0400

    refac: removed icon

--- src/components/SearchSection/SearchSection.tsx
+++ src/components/SearchSection/SearchSection.tsx
@@ -1,17 +1,16 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Input from "../Input/Input";
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 export default function SearchSection() {
   return (
     <div className="flex items-center justify-center">
-      <FontAwesomeIcon icon={faSearch} />
-      <section className="w-fit h-16 p-2 flex items-center justify-center">
-        <Input
-          required={false}
-          type="text"
-          placeholder="Digite o nome do produto..."
-        />
+      <section className="w-fit h-16 p-2">
+        <form action="/">
+          <Input
+            required={false}
+            type="text"
+            placeholder="Digite o nome do produto..."
+          />
+        </form>
       </section>
     </div>
   );
