73536576002bf734d2ee493e7a72314b91e12b22
Marden Rafael <mardenrafaeldalmagro.gimenez@gmail.com>
2023-06-01 00:24:04 -0400

    refac: removed navbar from header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,7 +7,6 @@ export interface HeaderProps {
 export default function Header({ title }: HeaderProps): JSX.Element {
   return (
     <header className="flex flex-col items-center">
-      <NavBar />
       <section className="w-full p-4 h-40 flex items-center justify-center">
         <h1>{title}</h1>
       </section>
