5547b01c81a79e343c8b659dbffd8fcb6fcb4d61
Marden Rafael <mardenrafaeldalmagro.gimenez@gmail.com>
2023-06-01 22:19:52 -0400

    refac: removed useless if statements

--- src/components/Anchor/Anchor.tsx
+++ src/components/Anchor/Anchor.tsx
@@ -3,34 +3,23 @@ import { PropsWithChildren } from "react";
 export interface AnchorProps extends PropsWithChildren {
   href: string;
   className?: string;
-  extend?: boolean;
   target?: string;
-  blankStyle?: boolean;
 }
 
 export default function Anchor({
   href,
   className,
-  extend,
+
   target,
-  blankStyle,
+
   children,
 }: AnchorProps): JSX.Element {
-  const DEFAULT_CLASS_NAME =
-    "font-medium text-primary-600 hover:underline dark:text-primary-500 ";
-
   return (
     <a
       href={href}
       target={target}
       className={
-        blankStyle
-          ? ""
-          : className
-          ? extend
-            ? DEFAULT_CLASS_NAME + className
-            : className
-          : DEFAULT_CLASS_NAME
+        className ? className : "font-medium text-primary-600 hover:underline "
       }
     >
       {children}
