631c576d39934bc260fffb5e09fe605c454da6d1
Marden Rafael <mardenrafaeldalmagro.gimenez@gmail.com>
2023-06-02 22:23:31 -0400

    chore: create start.sh

---
+++ cmd/start.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+echo "#############################"
+echo "# Processo de inicialização #"
+echo "#############################"
+/usr/local/bin/docker-entrypoint.sh
+
+
+echo "#####################################################"
+echo "# Reiniciando banco de dados e aplicando migrations #"
+echo "#####################################################"
+npx prisma migrate reset -f
+
+
+echo "#######################"
+echo "# Iniciando o sistema #"
+echo "#######################"
+npm run dev
