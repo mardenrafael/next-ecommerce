7cc68f689d76623c010fd6663c423192fff673d7
Marden Rafael <mardenrafaeldalmagro.gimenez@gmail.com>
2023-06-03 00:49:33 -0400

    refac: temp removed localstorage feature

--- src/context/themeContext.tsx
+++ src/context/themeContext.tsx
@@ -31,26 +31,8 @@ export default function ThemeProvider({
   function toggleTheme(): void {
     const newTheme =
       theme === ThemeOptions.light ? ThemeOptions.dark : ThemeOptions.light;
-    const location = window.location;
-
-    localStorage.setItem(
-      location.hostname + ".user.preferred.theme",
-      JSON.stringify({ theme: newTheme })
-    );
     setTheme(newTheme);
   }
-
-  useEffect(() => {
-    const location = window.location;
-
-    const userPreferredTheme = localStorage.getItem(
-      location.hostname + ".user.preferred.theme"
-    );
-
-    if (userPreferredTheme) {
-      setTheme(JSON.parse(userPreferredTheme).theme);
-    }
-  }, []);
 
   return (
     <ThemeContext.Provider
