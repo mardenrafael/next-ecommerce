92f45aac0e5dee82a99752b6394dea18b3f764da
Marden Rafael <mardenrafaeldalmagro.gimenez@gmail.com>
2023-06-06 23:28:34 -0400

    refac: atualiza camada de persistencia com typeORM

--- src/dao/UserDao/UserDao.ts
+++ src/dao/UserDao/UserDao.ts
@@ -1,32 +1,44 @@
-import TypeORMConnector from "@/database/connection/TypeOrmConnector";
-// import { AppDataSource } from "@/database/data-source";
 import { User } from "@/database/model/User";
 import NotFoundError from "@/errors/NotFoundError";
-import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
 import { randomUUID } from "crypto";
 import Dao from "../Dao";
+import {
+  Equal,
+  FindManyOptions,
+  FindOptionsRelations,
+  FindOptionsSelect,
+} from "typeorm";
+import { genSalt, hash } from "bcryptjs";
 
 export default class UserDao extends Dao<User> {
   constructor() {
     super();
   }
 
-  public async create({ email, name, password, terms }: User): Promise<User> {
-    const connector = TypeORMConnector.getInstance();
+  public async create({
+    email,
+    name,
+    password,
+    terms,
+  }: Omit<User, "id">): Promise<User> {
+    const connector = super.getConnector();
 
     try {
       await connector.establish();
 
       const appDataSource = connector.getDataSource();
+      const userRepository = appDataSource.getRepository(User);
 
       const uuid = randomUUID();
-      const userRepository = appDataSource.getRepository(User);
+
+      const salt = await genSalt(12);
+      const hashedPassword = await hash(password, salt);
 
       const user: User = new User();
       user.id = uuid;
       user.email = email;
       user.name = name;
-      user.password = password;
+      user.password = hashedPassword;
       user.terms = terms;
 
       const savedUser = await userRepository.save(user);
@@ -35,56 +47,74 @@ export default class UserDao extends Dao<User> {
     } catch (error: unknown) {
       console.error(error);
 
-      if (error instanceof PrismaClientKnownRequestError) {
-        throw new Error("Database error");
-      }
-
       throw error;
     } finally {
       await connector.close();
     }
   }
 
-  public async getById(id: String): Promise<User> {
-    try {
-      // const userRepository = AppDataSource.getRepository(User);
-      // const user = await userRepository.findOneBy({
-      // id: Equal(id),
-      // });
+  public async getById(id: string): Promise<User> {
+    const connector = super.getConnector();
 
-      // if (user == null) {
-      //   throw new NotFoundError();
-      // }
-      return new User();
-    } catch (error: unknown) {
-      if (error instanceof NotFoundError) {
-        throw new Error("Usuario não encontrado");
+    try {
+      await connector.establish();
+
+      const appDataSource = connector.getDataSource();
+      const userRepository = appDataSource.getRepository(User);
+
+      const userOrNull = await userRepository.findOne({
+        where: {
+          id: Equal(id),
+        },
+      });
+
+      if (userOrNull == null) {
+        throw new NotFoundError();
       }
+
+      return userOrNull;
+    } catch (error: unknown) {
       throw error;
+    } finally {
+      await connector.close();
     }
   }
   public async getBy(
     atribute: keyof User,
-    value: string | boolean
-  ): Promise<User> {
+    value: string | boolean,
+    join?: FindOptionsRelations<User>,
+    select?: FindOptionsSelect<User>
+  ): Promise<User[]> {
+    const connector = super.getConnector();
+
     try {
-      const connector = super.getConnector();
-      // // const prisma = connector.getPrismaclientInstance();
+      await connector.establish();
 
-      // const user = await prisma.$queryRaw<User>`
-      //     SELECT * FROM users
-      //       WHERE 1 = 1
-      //       AND email = ${value};`;
+      const appDataSource = connector.getDataSource();
+      const userRepository = appDataSource.getRepository(User);
 
-      // console.log(user);
+      const findManyOptions: FindManyOptions = {
+        where: {
+          [atribute]: Equal(value),
+        },
+        select: {},
+      };
 
-      return new User();
-    } catch (error: unknown) {
-      if (error instanceof PrismaClientKnownRequestError) {
-        throw new Error("Data base error");
+      if (join != undefined) {
+        findManyOptions.relations = join;
       }
 
+      if (select != undefined) {
+        findManyOptions.select = select;
+      }
+
+      const users = await userRepository.find(findManyOptions);
+
+      return users;
+    } catch (error: unknown) {
       throw error;
+    } finally {
+      connector.close();
     }
   }
 }
