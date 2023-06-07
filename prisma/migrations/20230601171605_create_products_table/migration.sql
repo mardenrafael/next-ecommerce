0ffcff8994a9c96cec42094cace8f2ea421ef8be
Marden Rafael <mardenrafaeldalmagro.gimenez@gmail.com>
2023-06-06 23:33:00 -0400

    refac: remove prisma por completo

--- prisma/migrations/20230601171605_create_products_table/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "products" (
-    "pro_id" UUID NOT NULL,
-    "pro_name" TEXT NOT NULL,
-    "pro_image" TEXT NOT NULL,
-
-    CONSTRAINT "products_pkey" PRIMARY KEY ("pro_id")
-);
