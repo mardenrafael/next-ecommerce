c09682818924ea50a2841d3619e5e75cca1e6391
Marden Rafael <mardenrafaeldalmagro.gimenez@gmail.com>
2023-06-06 23:29:18 -0400

    refac: atualiza Response

--- src/http/Response.ts
+++ src/http/Response.ts
@@ -1,13 +1,7 @@
 import { User } from "@/database/model/User";
-import { InternalServerErrorInterface } from "@/errors/InternalServerError";
-import { MethodNotAllowedInterface } from "@/errors/MethodNotAllowedError";
-import { UnsupportedMediaTypeInterface } from "@/errors/UnsupportedMediaType";
 
 export default interface Response {
   message?: string;
-  error?:
-    | MethodNotAllowedInterface
-    | UnsupportedMediaTypeInterface
-    | InternalServerErrorInterface;
+  error?: Error;
   data?: User[];
 }
