bf14013f5bd62f51f3e3202b98ee5178fb8dfdb5
Marden Rafael <mardenrafaeldalmagro.gimenez@gmail.com>
2023-06-06 21:22:09 -0400

    refac: Change TypeORMConnector

--- src/dao/UserDao/UserDao.ts
+++ src/dao/UserDao/UserDao.ts
@@ -1,11 +1,10 @@
-import NotFoundError from "@/errors/NotFoundError";
-
-import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
-import Dao from "../Dao";
+import TypeORMConnector from "@/database/connection/TypeOrmConnector";
+// import { AppDataSource } from "@/database/data-source";
 import { User } from "@/database/model/User";
+import NotFoundError from "@/errors/NotFoundError";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
 import { randomUUID } from "crypto";
-import { AppDataSource } from "@/database/data-source";
-import { Equal } from "typeorm";
+import Dao from "../Dao";
 
 export default class UserDao extends Dao<User> {
   constructor() {
@@ -13,10 +12,15 @@ export default class UserDao extends Dao<User> {
   }
 
   public async create({ email, name, password, terms }: User): Promise<User> {
-    const a = await AppDataSource.initialize();
+    const connector = TypeORMConnector.getInstance();
+
     try {
+      await connector.establish();
+
+      const appDataSource = connector.getDataSource();
+
       const uuid = randomUUID();
-      const userRepository = AppDataSource.getRepository(User);
+      const userRepository = appDataSource.getRepository(User);
 
       const user: User = new User();
       user.id = uuid;
@@ -37,21 +41,21 @@ export default class UserDao extends Dao<User> {
 
       throw error;
     } finally {
-      await a.destroy();
+      await connector.close();
     }
   }
 
   public async getById(id: String): Promise<User> {
     try {
-      const userRepository = AppDataSource.getRepository(User);
-      const user = await userRepository.findOneBy({
-        id: Equal(id),
-      });
+      // const userRepository = AppDataSource.getRepository(User);
+      // const user = await userRepository.findOneBy({
+      // id: Equal(id),
+      // });
 
-      if (user == null) {
-        throw new NotFoundError();
-      }
-      return user;
+      // if (user == null) {
+      //   throw new NotFoundError();
+      // }
+      return new User();
     } catch (error: unknown) {
       if (error instanceof NotFoundError) {
         throw new Error("Usuario não encontrado");
@@ -65,16 +69,16 @@ export default class UserDao extends Dao<User> {
   ): Promise<User> {
     try {
       const connector = super.getConnector();
-      const prisma = connector.getPrismaclientInstance();
+      // // const prisma = connector.getPrismaclientInstance();
 
-      const user = await prisma.$queryRaw<User>`
-          SELECT * FROM users
-            WHERE 1 = 1
-            AND email = ${value};`;
+      // const user = await prisma.$queryRaw<User>`
+      //     SELECT * FROM users
+      //       WHERE 1 = 1
+      //       AND email = ${value};`;
 
-      console.log(user);
+      // console.log(user);
 
-      return user as User;
+      return new User();
     } catch (error: unknown) {
       if (error instanceof PrismaClientKnownRequestError) {
         throw new Error("Data base error");
