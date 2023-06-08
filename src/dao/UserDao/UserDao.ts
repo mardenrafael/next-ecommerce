133632d9e99c10a16244815185ed60db29c7b6e9
Marden Rafael <mardenrafaeldalmagro.gimenez@gmail.com>
2023-06-08 02:48:37 -0400

    feat: implementa prisma

--- src/dao/UserDao/UserDao.ts
+++ src/dao/UserDao/UserDao.ts
@@ -1,7 +1,7 @@
-import NotFoundError from "@/http/errors/NotFoundError";
 import { randomUUID } from "crypto";
 import Dao from "../Dao";
 
+import { User } from "@prisma/client";
 import { genSalt, hash } from "bcryptjs";
 
 export default class UserDao extends Dao<User> {
@@ -14,8 +14,9 @@ export default class UserDao extends Dao<User> {
     name,
     password,
     terms,
-  }: Omit<User, "id">): Promise<User> {
+  }: Omit<User, "id">): Promise<Omit<User, "password">> {
     const connector = super.getConnector();
+    const prisma = connector.getPrisma();
 
     try {
       const uuid = randomUUID();
@@ -23,14 +24,24 @@ export default class UserDao extends Dao<User> {
       const salt = await genSalt(12);
       const hashedPassword = await hash(password, salt);
 
-      const user: User = new User();
-      user.id = uuid;
-      user.email = email;
-      user.name = name;
-      user.password = hashedPassword;
-      user.terms = terms;
-
-      // return savedUser;
+      const [savedUser] = await prisma.$transaction([
+        prisma.user.create({
+          data: {
+            id: uuid,
+            email,
+            name,
+            password: hashedPassword,
+            terms,
+          },
+          select: {
+            email: true,
+            id: true,
+            name: true,
+            terms: true,
+          },
+        }),
+      ]);
+      return savedUser;
     } catch (error: unknown) {
       console.error(error);
 
@@ -38,11 +49,26 @@ export default class UserDao extends Dao<User> {
     }
   }
 
-  public async getById(id: string): Promise<User> {
+  public async getById(id: string): Promise<Omit<User, "password">> {
     const connector = super.getConnector();
-
+    const prisma = connector.getPrisma();
     try {
-      // return userOrNull;
+      const user = await prisma.user.findUnique({
+        where: {
+          id,
+        },
+        select: {
+          email: true,
+          id: true,
+          name: true,
+          terms: true,
+        },
+      });
+
+      if (user == null) {
+        throw new Error();
+      }
+      return user;
     } catch (error: unknown) {
       throw error;
     }
