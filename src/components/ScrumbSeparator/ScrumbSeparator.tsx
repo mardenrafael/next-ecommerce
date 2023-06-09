a0aff363c79cf4c965d22554757746a61e37e56a
Marden Rafael <mardenrafaeldalmagro.gimenez@gmail.com>
2023-06-09 00:17:29 -0400

    feat: adiciona tema

--- src/components/ScrumbSeparator/ScrumbSeparator.tsx
+++ src/components/ScrumbSeparator/ScrumbSeparator.tsx
@@ -1,6 +1,16 @@
+import { ThemeContext, ThemeOptions } from "@/context/themeContext";
 import { faCaretRight } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useContext } from "react";
 
 export default function ScrumbSeparator(): JSX.Element {
-  return <FontAwesomeIcon icon={faCaretRight} />;
+  const { theme } = useContext(ThemeContext);
+  return (
+    <FontAwesomeIcon
+      icon={faCaretRight}
+      className={
+        theme == ThemeOptions.light ? "text-gray-400" : "text-gray-700"
+      }
+    />
+  );
 }
