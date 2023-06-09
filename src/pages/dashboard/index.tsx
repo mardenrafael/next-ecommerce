b8422b5ac7404771775dd04ff492922189967074
Marden Rafael <mardenrafaeldalmagro.gimenez@gmail.com>
2023-06-08 22:54:33 -0400

    feat: cria breadScrum

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,5 +1,14 @@
+import BreadScrum from "@/components/BreadScrum/BreadScrum";
 import SideNav from "@/components/SideNav/SideNav";
 
 export default function Dashboard(): JSX.Element {
-  return <SideNav logoUrl="https://flowbite.com/docs/images/logo.svg" />;
+  return (
+    <main className="flex h-screen">
+      <SideNav logoUrl="https://flowbite.com/docs/images/logo.svg" />
+
+      <div className="h-fit ml-2">
+        <BreadScrum />
+      </div>
+    </main>
+  );
 }
