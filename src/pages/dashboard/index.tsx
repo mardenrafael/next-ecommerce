4cae2d0943fde2eba3f0eb7dc9b4331f9af45e22
Marden Rafael <mardenrafaeldalmagro.gimenez@gmail.com>
2023-06-09 00:11:11 -0400

    feat: adiciona BreadScrumbs e Footer

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,14 +1,21 @@
-import BreadScrum from "@/components/BreadScrum/BreadScrum";
+import BreadScrum from "@/components/BreadScrumbs/BreadScrumbs";
+import Container from "@/components/Container/Container";
+import Footer from "@/components/Footer/Footer";
 import SideNav from "@/components/SideNav/SideNav";
 
 export default function Dashboard(): JSX.Element {
   return (
-    <main className="flex h-screen">
-      <SideNav logoUrl="https://flowbite.com/docs/images/logo.svg" />
+    <>
+      <main className="flex">
+        <Container>
+          <SideNav logoUrl="https://flowbite.com/docs/images/logo.svg" />
+        </Container>
 
-      <div className="h-fit ml-2">
-        <BreadScrum />
-      </div>
-    </main>
+        <nav className="h-fit ml-2">
+          <BreadScrum />
+        </nav>
+      </main>
+      <Footer />
+    </>
   );
 }
